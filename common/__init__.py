29e3ec4e4f562babbdd61babce3a04fcb0289b17
CongYang <39213788+yangcong356@users.noreply.github.com>
2024-06-07 14:45:38 +0800

    Add files via upload

---
+++ common/__init__.py
@@ -0,0 +1,8 @@
+# __all__ = ['config','dist_utils', 'logger', 'optims', 'registry', 'utils']
+
+from .config import *
+from .dist_utils import *
+from .logger import *
+from .optims import *
+from .registry import *
+from .utils import *
